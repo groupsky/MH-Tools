175df7503aae2e56b17b2c7422e0b314863c0ca8
Geno Roupsky <geno@roupsky.name>
2017-12-17 12:53:37 +0200

    Fix population extraction and update to latest nightly

--- tools/gwh2017pop.js
+++ tools/gwh2017pop.js
@@ -2,6 +2,16 @@
 
 var jt = require('jacksmhtools-client')
 var utils = require('./_utils')
+var stages = [
+  'Snow (1)', 'Wrapping Paper (2)', 'Yule Log (3)', 'Eggnog (4)', 'Tree (5)', 'Scarf (6)',
+  'Gingerbread (7)', 'Peppermint (8)', 'Ice (9)', 'Nougat (10)', 'Core'
+]
+var cheeses = [
+  'SB+', 'Gouda', 'Brie', 'Pecan Pecorino', 'Arctic Asiago'
+]
+var charms = [
+  undefined, 'Winter', 'Let It Snow'
+]
 
 utils
   .process({
@@ -9,13 +19,8 @@ utils
     series: [
       {
         location: utils.genVarField('location', 'Festive Comet'),
-        phase: utils.genVarField('stage', [
-          'Snow (1)', 'Wrapping Paper (2)', 'Yule Log (3)', 'Eggnog (4)', 'Tree (5)', 'Scarf (6)',
-          'Gingerbread (7)', 'Peppermint (8)', 'Ice (9)', 'Core'
-        ]),
-        cheese: utils.genVarField('cheese', [
-          'Gouda', 'SB+', 'Arctic Asiago', 'Pecan Pecorino', 'Brie'
-        ]),
+        phase: utils.genVarField('stage', stages),
+        cheese: utils.genVarField('cheese', cheeses),
         charm: [
           { vars: { charm: { 'Winter': false, 'Let It Snow': false } } },
           { vars: { charm: { 'Winter': true } }, fields: { charm: 'Winter' } },
@@ -26,11 +31,17 @@ utils
     process: function (item) {
       console.error('requesting', JSON.stringify(item.vars))
       return jt.getSAEncounterRateData(item.vars, item.opts)
-        .filter(function (item) {
-          return item.sample > 100
-        })
+        .filter(function (i) { return item.fields.stage === 'Core' || i.sample > 100 })
         .map(utils.preparePopulation.bind(utils, item.fields))
     }
+  })
+  .then(function (items) {
+    return items.sort(function (a, b) {
+      return (stages.indexOf(a.stage) - stages.indexOf(b.stage)) * 10000
+        + (cheeses.indexOf(a.cheese) - cheeses.indexOf(b.cheese)) * 1000
+        + (charms.indexOf(a.charm) - charms.indexOf(b.charm)) * 100
+        + parseFloat(b.attraction) - parseFloat(a.attraction)
+    })
   })
   .then(utils.toCsv.bind(utils, utils.POP_FIELDS))
   .then(console.log.bind(console))
